b80dd6e79468cf01c53c47d87be4e306ee081469
joshua <joshua.brionne@gmail.com>
2022-02-19 11:58:05 +0100

    [FIX] Creation.

---
+++ api/controllers/actions.js
@@ -0,0 +1,94 @@
+var db = require('../config/database.js');
+
+var colors = ['red', 'green', 'blue', 'orange', 'yellow', 'cyan', 'AntiqueWhite', 'BlueViolet', 'Crimson', 'DarkOrange'];
+
+colors[Math.floor(Math.random() * colors.length)];
+
+function check_validity(elem) {
+
+    if (elem == undefined || elem.length == 0)
+        return 0
+    return 1
+}
+
+exports.add = (req, res, next) => {
+    const name = req.body.name
+    const action = req.body.action
+    const block = req.body.block
+    const code = req.body.code
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    if (check_validity(name) && check_validity(action) && check_validity(block) && check_validity(color)) {
+        db.execute('SELECT * FROM `code_digit` WHERE code = (?)', [code], function (err, results, fields) {
+            if (results[0]) {
+                db.execute('INSERT INTO `actions` (name, color, action, block, code) VALUES (?, ?, ?, ?, ?)', [name, color, action, block, code], function (err, results, fields) {
+                    if (!err) {
+                        res.status(201).json({ name: name, color: color, action: action, block: block, id: results.insertId });
+                    }
+                    else
+                        res.status(400).json({ msg: "error processing" });
+                })
+            } else {
+                res.st0atus(400).json({ msg: "Unknown code" });
+            }
+
+        })
+    }
+}
+
+exports.getAllActions = (req, res, next) => {
+    db.execute('SELECT * FROM `actions`', function (err, results, fields) {
+        if (!err) {
+            res.status(200).json(results);
+            return
+        }
+        else
+            res.status(400).json({ error: "error" });
+    })
+}
+
+exports.getAllAutomation = (req, res, next) => {
+    const code = req.body.code
+
+    if (check_validity(code)) {
+        db.execute('SELECT id, name, color, action, block, is_active FROM `actions` WHERE code = ?', [code], function (err, results, fields) {
+            if (results[0]) {
+                res.status(202).json(results);
+            }
+            else
+                res.status(400).json({ msg: "error processing" });
+        })
+
+    }
+}
+exports.trigger_up = (req, res, next) => {
+    const code = req.body.code
+    const id = req.body.id
+
+    if (check_validity(id) && check_validity(code)) {
+        db.execute('UPDATE `actions` SET is_active = 1 WHERE  code = ? AND id = ?', [code, id], function (err, results, fields) {
+            if (results.affectedRows != 0) {
+                res.status(200).json({ msg: "ok" });
+            }
+            else
+                res.status(400).json({ msg: "error processing" });
+        })
+
+    }
+}
+
+exports.trigger_down = (req, res, next) => {
+    const code = req.body.code
+    if (check_validity(code)) {
+        db.execute('SELECT id, actions FROM `actions` WHERE  code = ? AND is_active = 1', [code], function (err, results, fields) {
+            const row = results
+            db.execute('UPDATE `actions` SET is_active = 0 WHERE code = ? AND is_active = 1', [code], function (err, results, fields) {
+                if (results.affectedRows != 0) {
+                    res.status(200).json( row );
+                }
+                else
+                    res.status(400).json({ msg: "error processing" });
+            })
+        })
+    }
+}
---
+++ api/routes/actions.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const actionCtrl = require('../controllers/actions')
+
+router.post('/add', actionCtrl.add)
+router.get('/', actionCtrl.getAllActions)
+router.get('/automations', actionCtrl.getAllAutomation)
+router.put('/trigger_up', actionCtrl.trigger_up)
+router.put('/trigger_down', actionCtrl.trigger_down)
+
+module.exports = router;  
