819779b3167ee396ac6858a7d98292236c543208
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-02-19 19:26:23 +0100

    localStorage + registration

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,4 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+const fetch = require('node-fetch');
+var LocalStorage;
+var localStorage;
+
+if (typeof localStorage === 'undefined' || localStorage === null) {
+    LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./storage');
+}
 
 const createWindow = (width, height) => {
     const window = new BrowserWindow({
@@ -7,6 +15,7 @@ const createWindow = (width, height) => {
         height: height,
         minWidth: 800,
         minHeight: 600,
+        title: 'BTA Client',
         icon: __dirname + '/assets/icon_bta.png',
         webPreferences: {
             nodeIntegration: true
@@ -17,7 +26,20 @@ const createWindow = (width, height) => {
 
 app.whenReady().then(() => {
     const window = createWindow(1280, 800);
-    window.loadFile('static/index.html');
+
+    if (localStorage.getItem("code"))
+        window.loadFile('static/index.html');
+    else {
+        fetch('https://api-bta.tk/auth/register', {
+            method: 'POST'
+        }).then((response) => {
+            response.text().then((text) => {
+                localStorage.setItem('code', JSON.parse(text).code);
+            });
+        }).then(() => {
+            window.loadFile('static/index.html');
+        });
+    }
 });
 
 app.on('window-all-closed', () => {
---
+++ desktop/src/static/tailwind_theme/tailwind.condig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'navbar_color': '#252525',
+                'dark': '#141414',
+            }
+        }
+    }
+}
