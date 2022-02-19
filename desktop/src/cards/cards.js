83e274cb552ee822528d88e2eb0995faea8843fb
MathiDEV <mathias.andre@epitech.eu>
2022-02-19 11:14:54 +0100

    [FEAT] Added fontawesome.

---
+++ desktop/src/cards/cards.js
@@ -0,0 +1,17 @@
+function cards(card) {
+    return html`
+    <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+        <div class="flex justify-center md:justify-end -mt-16">
+        </div>
+        <div>
+            <h2 class="text-gray-800 text-3xl font-semibold">Design Tools</h2>
+            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea
+                doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus
+                consequuntur!</p>
+        </div>
+        <div class="flex justify-end mt-4">
+            <a href="#" class="text-xl font-medium text-indigo-500">John Doe</a>
+        </div>
+    </div>
+    `
+}
