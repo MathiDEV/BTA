e5805bfeea3f47d5326edb64964f9690ab51fc53
joshua <joshua.brionne@gmail.com>
2022-02-19 17:54:13 +0100

    [FIX] fixing some stuffs

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -22,7 +22,7 @@ exports.getAllCodes = (req, res, next) => {
             return
         }
         else
-            res.status(400).json({ err });
+            res.status(400).json({ error: "error" });
     })
 }
 
